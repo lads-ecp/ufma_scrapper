d32461694f268347691aada2e488a637fa6fc307
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2019-11-22 16:58:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name = 'ufma_scrapperpy',       
-  packages = ['ufma_scrapperpy'],   # Chose the same as "name"
+  name = 'ufma_scrapper',       
+  packages = ['ufma_scrapper'],   # Chose the same as "name"
   version = '0.0.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
@@ -14,7 +14,7 @@ setup(
   author_email = 'profsergiocosta@gmail.com',      # Type in your E-Mail
  long_description = long_description ,
  long_description_content_type = "text/markdown",
-url = 'https://github.com/inovacampus/ufma_scrapperpy',   # Provide either the link to your github or to your website
+url = 'https://github.com/inovacampus/ufma_scrapper',   # Provide either the link to your github or to your website
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
    classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
